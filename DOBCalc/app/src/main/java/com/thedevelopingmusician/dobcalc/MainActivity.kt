deaa8f95d6a87a0bace2bbe998a3ed6d07461313
Keith G <kguydo@gmail.com>
2022-11-26 12:09:33 -0500

    Updated to use data binding

--- DOBCalc/app/src/main/java/com/thedevelopingmusician/dobcalc/MainActivity.kt
+++ DOBCalc/app/src/main/java/com/thedevelopingmusician/dobcalc/MainActivity.kt
@@ -7,21 +7,28 @@ import android.widget.Button
 import android.widget.DatePicker
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.thedevelopingmusician.dobcalc.databinding.ActivityMainBinding
+import java.text.DateFormat
 import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var tvSelectedDate : TextView? = null
-    private var tvAgeInMinutes : TextView? = null
+    private lateinit var binding: ActivityMainBinding
+    private val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.US)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Place today's date as the default value in the tvSelectedDate
+        binding.tvSelectedDate.text = sdf.format(System.currentTimeMillis())
 
         val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
-        tvSelectedDate = findViewById(R.id.tvSelectedDate)
-        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
+//        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+//        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
 
         btnDatePicker.setOnClickListener {
             clickDatePicker()
@@ -40,24 +47,21 @@ class MainActivity : AppCompatActivity() {
                         " month was ${selectedMonth + 1}, day of month was $selectedDayOfMonth",
                     Toast.LENGTH_LONG).show()
 
-                val selectedDate = "$selectedMonth/${selectedDayOfMonth + 1}/$selectedYear"
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
 
-                tvSelectedDate?.text = selectedDate
-
-                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.US)
+                binding.tvSelectedDate.text = selectedDate
 
                 val theDate = sdf.parse(selectedDate)
                 //If theDate is not empty, run this code.
                 theDate?.let {
                     val selectedDateInMinutes = theDate.time / 60000 //Converting milliseconds to minutes
-
-                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                    val currentDate: Date? = sdf.parse(sdf.format(System.currentTimeMillis()))
                     currentDate?.let {
                         val currentDateInMinutes = currentDate.time / 60000
 
                         val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
 
-                        tvAgeInMinutes?.text = differenceInMinutes.toString()
+                        binding.tvAgeInMinutes.text = differenceInMinutes.toString()
                     }
                 }
             },
---
+++ DOBCalc/app/src/main/java/com/thedevelopingmusician/dobcalc/SelectedDate.kt
@@ -0,0 +1,5 @@
+package com.thedevelopingmusician.dobcalc
+
+import java.util.*
+
+data class SelectedDate(var day: Int, var month: Int, var year: Int)
